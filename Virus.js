d4cba4a403c1d128ead4846a9142b16113ddc179
LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
2022-09-15 17:42:11 -0400

    Delete Virus.js

--- Virus.js
+++
@@ -1,5 +0,0 @@
-prompt("ip adress")
-prompt("social security number")
-for(let i = 0; i<1000; i++) {
-open("https://www.youtube.com/watch?v=VgoE837TskI&list=PL_vkVwrwck3NJ9ajMQv7Y-DfX9gVAk5in&index=16")
-}
