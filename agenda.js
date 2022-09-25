d6226d30803d01c80df7d5ee08aedae90954b8c6
LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
2022-09-25 15:53:00 -0400

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -1,13 +1,13 @@
 function asmtData() {
 let aname = document.getElementById('aname').value;
 
-let class = document.getElementById('class').value;
+let aclass = document.getElementById('class').value;
 
 let dueDate = document.getElementById('date').value;
 
 let dueTime = document.getElementById('time').value;
   
-localStorage.setItem(class+":"+aname+" -- "+dueDate+" "+dueTime+"!");location.reload();
+localStorage.setItem(aclass+":"+aname+" -- "+dueDate+" "+dueTime+"!");location.reload();
 }
 
 function writeAssignments() {
