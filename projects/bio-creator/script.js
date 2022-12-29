3a41680f4379339d0eb34df23ac5fa07596a03b1
LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
2022-12-29 10:08:31 -0600

    Projects (#38)
    * Create index.html
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
    
    * Create stylesheet.css
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
    
    * Create index.html
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
    
    * Create stylesheet.css
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
    
    * Create script.js
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
    
    * Update index.html
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>
    
    Signed-off-by: LeMoose6 <113636339+LeMoose6@users.noreply.github.com>

---
+++ projects/bio-creator/script.js
@@ -0,0 +1,60 @@
+const bioData = [];
+var bio;
+var contactInfo;
+		
+function values() {
+	bioData[0] = document.getElementById("name").value;
+	bioData[1] = document.getElementById("bday").value;
+	bioData[2] = document.getElementById("fact").value;
+	bioData[3] = document.getElementById("contact").value;
+	bioData[4] = document.getElementById("platform").value;
+}
+		
+function write() {
+	bio = ""
+			
+	if (bioData[0] != "") {
+		bio = "Hi, my name is " + bioData[0];
+	} else {
+		bioData = []
+	}
+			
+	if (bioData[1] != "") {
+		bio += ". My birthday is on " + bioData[1];
+	}
+			
+	if (bioData[2] != "") {
+		bio += ", and a fun fact about me is that " + bioData[2];
+	}
+			
+	if (bioData[3] != "") {
+		bio += ". You can contact me at " + bioData[3];
+		contactInfo = 1;
+	} else {
+		contactInfo = 0;
+	}
+			
+	if (contactInfo = 1 && bioData[4] != "") {
+		bio += " on " + bioData[4]
+	}
+			
+			
+	bio += "."
+			
+	if (bio == ".") {
+		bio = "Please enter the required values"
+	} 
+}
+		
+function create() {
+	values();
+	write();
+	document.getElementById("bio").innerHTML = bio; 
+}
+		
+function reset() {
+	document.getElementById("name").value = "";
+	document.getElementById("bday").value = "";
+	document.getElementById("fact").value = "";
+	document.getElementById("contact").value = "";
+}
