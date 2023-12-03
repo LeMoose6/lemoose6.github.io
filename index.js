52ee67b20217cc05cc85a61c2aac22ad9497019a
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2023-12-02 22:36:41 -0500

    Homepage (#1)
    * featured content section
    
    * add padding to the featured content section
    
    * reorganize the top row
    
    * add the news cards
    
    * add experimental javascript
    
    * make navigation buttons work
    
    * back buttons work
    
    * fix and redesign the js for the navbar
    
    * Upload judy mudds
    
    * add in progress warning
    
    * judy mudd stories open in a separate tab
    
    * make the homepage a [better] link
    
    * add collapse button to navbar
    
    * fix the url issue with the homepage
    
    * open judy mudd without button
    
    * news fix attempt
    
    * remove alert
    
    * Upload library rules document
    
    * Add library rules link to the library page
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+// Navigation
+window.onpopstate = function (event) {
+    change_pages(event.state.page);
+}
+
+function change_pages(page) {
+    // Clear the screen
+    document.querySelectorAll('.page').forEach(page => {
+        page.style.display = 'none';
+    });
+
+    // Add page to history
+    if (page === "home") {
+        history.pushState({ page: "home" }, "", "/");
+    } else {
+        history.pushState({ page: page }, "", `${page}`);
+    }
+
+    // Show page
+    document.querySelector(`#${page}`).style.display = 'block';
+
+    if (page === 'wishlist') {
+        alert("You are going to be redirected to the wishlist");
+        open("https://mywishlist.online/w/bzi4an/nils-fotis");
+    }
+    // If the page is the library, redirect the user to the library website
+    else if (page === 'library') {
+        alert("You are going to be redirected to the library");
+        open("https://librarycat.org/lib/lemoose6")
+    }
+
+}
+
+function nav_buttons() {
+    // Select all the links, then give them a response to a click
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', () => {
+            change_pages(link.dataset.page);
+        });
+    });
+}
+
+function featured_content_clickable() {
+    document.querySelectorAll('.carousel-item').forEach(item => {
+        item.addEventListener('click', () => {
+            open(item.dataset.link)
+        });
+    });
+}
+
+function latest_news() {
+    // Get most recent non featured
+    var i = 0
+    while (true) {
+        if (UPDATES[i].featured !== true) {
+            document.querySelector('#latest-date').innerHTML += UPDATES[i].date;
+            document.querySelector('#latest-content').innerHTML = UPDATES[i].content;
+            break
+        }
+        i++
+        
+    }
+    i = 0
+    while (true) {
+        if (UPDATES[i].featured === true) {
+            document.querySelector('#featured-date').innerHTML += UPDATES[i].date;
+            document.querySelector('#featured-content').innerHTML = UPDATES[i].content;
+            break
+        }
+        i++
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>{change_pages("home");
+    // Run starting functions
+    nav_buttons();
+    featured_content_clickable();
+    latest_news();
+
+    // In Progress Warning
+alert("Welcome to the LeMoose Website! Our Judy Mudd and News pages are currently still under development. These features will be prepared before March 2024, thank you for your understanding!");
+});
