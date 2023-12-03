c4a049e56962824dd3291ac65edda26dd05c2ec1
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2023-12-03 15:18:26 -0500

    New post

--- news-articles.js
+++ news-articles.js
@@ -1,4 +1,5 @@
 const UPDATES = [
-    {"title": "2023 Muddmas", "date": "December 1", "content": "Games are coming back to LeMoose on December 17 as an early Muddmas present!", "featured": false},
-    {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while your waiting for this year's to release later this month", "featured": true},
+    {"title": "Title Annoucment", "date": "December 10", "content": 'As a birthday present to the inspiration and helper of Judy Mudd, we are offically announcing the title of this years Muddmas speical, "Judy Saves Christmas". You can view the charcters in the featured contect section.', featured: true}, 
+    {"title": "Return of Games", "date": "December 1", "content": "Games are coming back to LeMoose on December 17 as an early Muddmas present!", "featured": false},
+    {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while you're waiting for this year's to release later this month.", "featured": true},
 ]
