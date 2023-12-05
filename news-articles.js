0d63ebacb9531f73e19e00792e3954fe96861ab4
lemoose6 <nilsfotis@gmail.com>
2023-12-05 16:32:54 -0500

    remove the stuff for testing

--- news-articles.js
+++ news-articles.js
@@ -1,8 +1,5 @@
 // Use the following JSON template for each "article": {"title": [title], "date": [date in Month, Day], "content":  [the actual annoucement], "featured": [bool - is it important], "mudd": [bool - does it relate to judy mudd]}
 const UPDATES = [
-    {"title": "Test 3", "date": "December 5", "content": "Hello!" , "featured": true, "mudd": true},
-    {"title": "Test 2", "date": "December 5", "content": "Hello!" , "featured": false, "mudd": true},
-    {"title": "Test", "date": "December 5", "content": "Hello!" , "featured": false, "mudd": true},
     {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while your waiting for this year's to release later this month", "featured": true, "mudd": true},
     {"title": "Games Returning", "date": "December 1", "content": "Games are coming back to LeMoose on December 17 as an early Muddmas present!", "featured": false, "mudd": false},
 ]
