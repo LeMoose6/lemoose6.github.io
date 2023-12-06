116e1f0014420408754b44add6579185792a4f06
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2023-12-05 21:04:12 -0500

    Update news-articles.js

--- news-articles.js
+++ news-articles.js
@@ -1,5 +1,6 @@
 // Use the following JSON template for each "article": {"title": [title], "date": [date in Month, Day], "content":  [the actual annoucement], "featured": [bool - is it important], "mudd": [bool - does it relate to judy mudd]}
 const UPDATES = [
+    {"title": "Title Annoucement", "date": "December 10", "content": "This year's Judy Mudd holiday special is called Judy Saves Christmas. View the cover art and character list now!", "featured": true, "mudd": true},
     {"title": "Upcoming Announcement", "date": "December 5", "content": "Stay tuned this weekend for an annoucment regarding this year's Judy Mudd special!", "featured": false, "mudd": true},
     {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while you're waiting for this year's to release later this month!", "featured": true, "mudd": true},
     {"title": "Games Returning", "date": "December 1", "content": "Games are coming back to LeMoose on December 17 as an early Muddmas present!", "featured": false, "mudd": false},
