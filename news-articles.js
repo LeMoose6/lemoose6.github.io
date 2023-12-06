a93c0f95a6b11a56d712bba09201b78ddfdc7b44
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2023-12-05 20:55:08 -0500

    Update JMR branch (#5)
    * Fix error that occurred when the wishlist was removed
    
    * remove wishlist in the navbar
    
    * News page (#4)
    
    * add sections for the news
    
    * update the articles
    
    * edit classes of the html
    
    * edit id names
    
    * set up JS function
    
    * add padding to the sections
    
    * finish function
    
    * adjust padding
    
    * adjust divs to make it scrollable
    
    * make it look better
    
    * add helpful stuff
    
    * remove the stuff for testing
    
    * Add an update

--- news-articles.js
+++ news-articles.js
@@ -1,5 +1,8 @@
+// Use the following JSON template for each "article": {"title": [title], "date": [date in Month, Day], "content":  [the actual annoucement], "featured": [bool - is it important], "mudd": [bool - does it relate to judy mudd]}
 const UPDATES = [
-    {"title": "Title Annoucment", "date": "December 10", "content": 'As a birthday present to the inspiration and helper of Judy Mudd, we are offically announcing the title of this years Muddmas speical, "Judy Saves Christmas". You can view the charcters in the featured contect section.', featured: true}, 
-    {"title": "Return of Games", "date": "December 1", "content": "Games are coming back to LeMoose on December 17 as an early Muddmas present!", "featured": false},
-    {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while you're waiting for this year's to release later this month.", "featured": true},
+    {"title": "Upcoming Announcement", "date": "December 5", "content": "Stay tuned this weekend for an annoucment regarding this year's Judy Mudd special!", "featured": false, "mudd": true},
+    {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while you're waiting for this year's to release later this month!", "featured": true, "mudd": true},
+    {"title": "Games Returning", "date": "December 1", "content": "Games are coming back to LeMoose on December 17 as an early Muddmas present!", "featured": false, "mudd": false},
 ]
+
+// template {"title": , "date": , "content": , "featured": false, "mudd": false},
