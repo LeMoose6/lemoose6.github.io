1997735c826d1be3f42e6815d4617278ccb39ce3
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2023-12-22 10:35:11 -0600

    Update news-articles.js

--- news-articles.js
+++ news-articles.js
@@ -1,5 +1,6 @@
 // Use the following JSON template for each "article": {"title": [title], "date": [date in Month, Day], "content":  [the actual annoucement], "featured": [bool - is it important], "mudd": [bool - does it relate to judy mudd]}
 const UPDATES = [
+    {"title": "Judy Saves Christmas", "date": "December 22", "content": "This year's muddmas special has officially released. Read Judy Saves Chirstmas NOW!", "featured": true, "mudd": true},
     {"title": "Title Annoucement", "date": "December 9", "content": "This year's Judy Mudd holiday special is called Judy Saves Christmas. View the cover art and character list now!", "featured": true, "mudd": true},
     {"title": "Upcoming Announcement", "date": "December 5", "content": "Stay tuned this weekend for an annoucment regarding this year's Judy Mudd special!", "featured": false, "mudd": true},
     {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while you're waiting for this year's to release later this month!", "featured": true, "mudd": true},
