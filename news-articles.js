3da40632dcbc9a94cef8bfbea8e78810a59402a9
Nielsen Fotis <113636339+LeMoose6@users.noreply.github.com>
2024-02-25 17:24:34 -0500

    Update news-articles.js

--- news-articles.js
+++ news-articles.js
@@ -1,10 +1,10 @@
-// Use the following JSON template for each "article": {"title": [title], "date": [date in Month, Day], "content":  [the actual annoucement], "featured": [bool - is it important], "mudd": [bool - does it relate to judy mudd]}
+// Use the following JSON template for each "article": {"title": [title], "date": [date in Month, Day], "content":  [the actual announcement], "featured": [bool - is it important], "mudd": [bool - does it relate to judy mudd]}
 const UPDATES = [
-    {"title": "Judy Saves Christmas", "date": "December 22", "content": "This year's muddmas special has officially released. Read Judy Saves Chirstmas NOW!", "featured": true, "mudd": true},
-    {"title": "Title Annoucement", "date": "December 9", "content": "This year's Judy Mudd holiday special is called Judy Saves Christmas. View the cover art and character list now!", "featured": true, "mudd": true},
-    {"title": "Upcoming Announcement", "date": "December 5", "content": "Stay tuned this weekend for an annoucment regarding this year's Judy Mudd special!", "featured": false, "mudd": true},
-    {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last years holiday special right now while you're waiting for this year's to release later this month!", "featured": true, "mudd": true},
-    {"title": "Games Returning", "date": "December 1", "content": "Games are coming back to LeMoose on January 10 as an early new year present!", "featured": false, "mudd": false},
+    {"title": "Judy Mudd Easter", "date": "February 25", "content":  "Judy Mudd returns this spring for Judy's Easter Special! More information coming soon.", "featured": true, "mudd": true}
+    {"title": "Judy Saves Christmas", "date": "December 22", "content": "This year's Muddmas special has officially released. Read Judy Saves Christmas NOW!", "featured": true, "mudd": true},
+    {"title": "Title Announcement", "date": "December 9", "content": "This year's Judy Mudd holiday special is called Judy Saves Christmas. View the cover art and character list now!", "featured": true, "mudd": true},
+    {"title": "Upcoming Announcement", "date": "December 5", "content": "Stay tuned this weekend for an announcement regarding this year's Judy Mudd special!", "featured": false, "mudd": true},
+    {"title": "2023 Muddmas", "date": "December 3", "content": "Muddmas is upon us. Read last year's holiday special right now while you're waiting for this year's to release later this month!", "featured": true, "mudd": true},
 ]
 
 // template {"title": , "date": , "content": , "featured": false, "mudd": false},
